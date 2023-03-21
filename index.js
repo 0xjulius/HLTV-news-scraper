6370e26d6f3cd75dd45cd616b9aef6681bbd4e3a
JULIUS AALTO <72438387+julmezha@users.noreply.github.com>
2023-03-21 03:42:12 +0200

    Initial commit
    Initial commit

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
+app.use(express.static('public'))   
+
+
+                                    //locating my frontend to vercel
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
+module.exports = app
 
 const url = 'https://www.hltv.org/'
 const baseUrl = 'https://www.hltv.org'
