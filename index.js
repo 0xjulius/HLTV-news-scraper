c1eca7770e5116542559e33dc1b164590bd3ced1
JULIUS AALTO <72438387+julmezha@users.noreply.github.com>
2023-03-21 02:57:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const axios = require('axios')      // webscraper
 const cheerio = require('cheerio')  // pickups certain HTML-elements
 const express = require('express')
